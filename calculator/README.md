fad1c134a5766bd00b9458324520110a279ee3e3
boahene <oseiwusuboahene@gmail.com>
2023-02-11 17:56:57 +0000

    a program to add

---
+++ calculator/README.md
@@ -0,0 +1,10 @@
+A c progam to sum up two inputs.
+
+first a user is asked to input a numnber,
+the program accepts the input and store
+
+secodnly the user is asked to input another number.
+the program takes the second input and stores it.
+
+finally the program sums up the number and display as sum.
+
---
+++ calculator/add.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+  int x;
+  int y;
+  int z;
+
+  printf("A Program to accept and add two numbers\n"); 
+  printf("hello, Please enter your first number\n"); //this part ask user to input the number
+  scanf("%d",&x);  //this line accept inputs
+  printf("Please enter another number\n");
+  scanf("%d",&y); 
+
+  z = x + y;   //the calculation happens here.
+
+  printf("your total number is %d\n",z);
+  printf("thanks for using this addition calcaulator\n");
+
+  return 0;
+}
