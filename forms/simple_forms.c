2a1d6f348ef06364d559b0ea71725ece184003c4
boahene <oseiwusuboahene@gmail.com>
2023-02-12 12:55:55 +0000

    file created

---
+++ forms/simple_forms.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+		int tel;
+		int id;
+		char fname;
+		char lname
+		char sex;
+		char loc;
+
+		//
+		*the program begins here,
+		* it starts with a request 
+		//
+
+		printf("***************CodeArena Forms*********************\n");
+		printf("Fill in the blank space to apply\n");
+		printf("what is your first name");
+		scanf("%d",&fname);
+		printf("what is your last name");
+		scanf("%d",&lname);
+		printf"Enter Your sex");
+		scanf("&d",&sex);
+		printf("what is your telephone number");
+		scanf("&d",&tel:
+
+
+		return 0;
+}
